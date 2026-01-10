929a0ae9ee1aa6dc3d6c374dc891a33e57170795
Dunay Gudratli <dqudretli@std.beu.edu.az>
2026-01-10 22:34:43 +0400

    200: implement smooth scrolling

--- script.js
+++ script.js
@@ -42,3 +42,19 @@ document
   .addEventListener('click', function () {
     message.remove();
   });
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+
+  window.scrollTo({
+    left: s1coords.left + window.scrollX,
+    top: s1coords.top + window.scrollY,
+    behavior: 'smooth',
+  });
+
+  //newer
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
